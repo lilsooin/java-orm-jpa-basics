ea3cbce5351487ac8fd7200f8125d13a2e453ae7
sooin <lilesooin@gmail.com>
2025-09-25 17:11:53 -0400

    프록시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,15 +15,30 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Book book = new Book();
-            book.setName("JPA");
-            book.setAuthor("김영한");
+            Member member1 = new Member();
+            member1.setName("member1");
+            em.persist(member1);
 
-            em.persist(book);
+            Member member2 = new Member();
+            member2.setName("member2");
+            em.persist(member2);
 
-            tx.commit();
-        } catch(Exception e) {
+            em.flush();
+            em.clear();
+
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass()); // proxy
+
+            em.detach(refMember);
+            // em.close();
+
+            refMember.getName();
+            emf.getPersistenceUnitUtil().isLoaded(refMember);
+
+                    tx.commit();
+        } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
